5a3c80b8b3bdea74c5f1865a3aee86c067a3d0fd
mark <markpackham1@gmail.com>
2022-08-02 12:08:27 +0100

    add props
    https://youtu.be/Hq47N56kmnQ?t=291

--- components demo/components/AppButton.js
+++ components demo/components/AppButton.js
@@ -1,13 +1,24 @@
 export default {
   template: `
-    <button class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed" :disabled="processing">
+    <button 
+    :class="{
+     'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed': true 
+    }" 
+    :disabled="processing">
         <slot />
         </button>
     `,
 
+  props: {
+    type: {
+      type: String,
+      default: "primary",
+    },
+  },
+
   data() {
     return {
-      processing: true,
+      processing: false,
     };
   },
 };
