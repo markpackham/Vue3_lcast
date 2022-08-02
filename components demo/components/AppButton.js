069402021e6ca1c78c31f740275e2e1e5c921006
mark <markpackham1@gmail.com>
2022-08-02 12:11:05 +0100

    setup button color types
    https://youtu.be/Hq47N56kmnQ?t=352

--- components demo/components/AppButton.js
+++ components demo/components/AppButton.js
@@ -2,7 +2,10 @@ export default {
   template: `
     <button 
     :class="{
-     'bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed': true 
+     'border rounded px-5 py-2 disabled:cursor-not-allowed': true,
+     'bg-gray-200 hover:bg-gray-400': type === 'muted',
+     'bg-blue-200 hover:bg-blue-400': type === 'primary',
+     'bg-green-200 hover:bg-green-400': type === 'secondary',
     }" 
     :disabled="processing">
         <slot />
