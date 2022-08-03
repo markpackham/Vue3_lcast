b6eb822dc89b28d14bc15f9a680682ae6043e379
mark <markpackham1@gmail.com>
2022-08-03 11:00:14 +0100

    move sections template into Assignments component
    https://youtu.be/Y1M67DdvHJo?t=282

--- Bringing It All Together/components/App.js
+++ Bringing It All Together/components/App.js
@@ -1,31 +1,4 @@
 export default {
-  template: `
-  <section>
-  <h2 class="font-bold mb-2">In Progress</h2>
-
-  <ul>
-      <li v-for="assignment in inProgressAssignments" :key="assignment.name">
-          <label>{{assignment.name}}
-
-              <input type="checkbox" v-model="assignment.complete">
-          </label>
-      </li>
-  </ul>
-</section>
-
-<section v-show="completedAssignments.length" class="mt-8">
-  <h2 class="font-bold mb-2">Completed</h2>
-
-  <ul>
-      <li v-for="assignment in completedAssignments" :key="assignment.name">
-          <label>{{assignment.name}}
-              <input type="checkbox" v-model="assignment.complete">
-          </label>
-      </li>
-  </ul>
-</section>
-  `,
-
   data() {
     return {
       assignments: [
