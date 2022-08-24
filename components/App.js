30dbd0b2da00404f6dba857aa1851d17df2806b4
mark <markpackham1@gmail.com>
2022-08-24 20:44:51 +0100

    add Panel to App
    https://youtu.be/_funL-qPp5Q?t=186

--- components/App.js
+++ components/App.js
@@ -1,11 +1,14 @@
 import Assignments from "./Assignments.js";
+import Panel from "./Panel.js";
 
 export default {
-  components: { Assignments },
+  components: { Assignments, Panel },
 
   template: `
   <div class="grid gap-6">
     <Assignments></Assignments>
+
+    <Panel heading="Hello World" />
   </div>
   `,
 };
--- components/Panel.js
+++ components/Panel.js
@@ -2,7 +2,12 @@ export default {
   template: `
   <div class="w-60 bg-gray-700 p-4 border border-gray-600 rounded-lg">
       <h2 class="text-xl font-bold mb-2">
+      {{ heading }}
       </h2>
   </div>
     `,
+
+  props: {
+    heading: String,
+  },
 };
