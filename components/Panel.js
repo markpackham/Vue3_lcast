685e2e4a8be8bfa53013333944a15f66c08f234f
mark <markpackham1@gmail.com>
2022-08-24 21:20:27 +0100

    set relevant text colors

--- components/Panel.js
+++ components/Panel.js
@@ -2,8 +2,8 @@ export default {
   template: `
   <div :class="{
     'w-60 p-4 border rounded-lg' : true,
-    'bg-white  border-gray-300' : theme == 'light'
-    'bg-gray-700  border-gray-600' : theme == 'dark',
+    'bg-white  border-gray-300 text-black' : theme == 'light',
+    'bg-gray-700  border-gray-600 text-white' : theme == 'dark',
   }">
 
       <h2 v-if="$slots.heading" class="text-xl font-bold mb-2">
